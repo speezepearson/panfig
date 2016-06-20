325ba56fa885dbd5bc6aeceb15f92d8ac50786ed
Spencer Pearson <suspense@cs.washington.edu>
2016-06-19 23:05:57 -0700

    be careful about decoding stdout/stderr

--- panfig/errors.py
+++ panfig/errors.py
@@ -47,8 +47,12 @@ def format_figure_failure(panfig_block, exception):
       traceback=indent(''.join(traceback.format_exception(exception.__class__, exception, exception.__traceback__))))
 
   if isinstance(exception, SubprocessFailed):
+    try: stdout = exception.out.decode()
+    except UnicodeDecodeError: stdout = exception.out
+    try: stderr = exception.err.decode()
+    except UnicodeDecodeError: stderr = exception.err
     result += addendum.format(
-        stdout=indent(exception.out.decode()),
-        stderr=indent(exception.err.decode()))
+        stdout=indent(stdout),
+        stderr=indent(stderr))
 
   return result
